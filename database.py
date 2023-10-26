96833b307afc4f9db8df3c672d5af2f888d91287
AVINASH0052 <86907203+AVINASH0052@users.noreply.github.com>
2023-10-26 16:27:39 +0000

    added connection to db

---
+++ database.py
@@ -0,0 +1,28 @@
+import pymysql.cursors
+import pymysql
+import os
+
+timeout = 10
+connection = pymysql.connect(
+  charset="utf8mb4",
+  connect_timeout=timeout,
+  cursorclass=pymysql.cursors.DictCursor,
+  db=os.environ['DB_NAME'],
+  host=os.environ['DB_HOST'],
+  password=os.environ['DB_PASSWORD'],
+  read_timeout=timeout,
+  port=int(os.environ['DB_PORT']),
+  user=os.environ['DB_USER'],
+  write_timeout=timeout,
+)
+
+def load_jobs_from_db():
+  jobs=[]
+  try:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM jobs")
+    for row in cursor.fetchall():
+      jobs.append(row)
+  finally:
+    connection.close()
+  return jobs
